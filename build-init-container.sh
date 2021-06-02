59f9ef02002425fd25b7079c7ebe884a80885f1c
kilmajster <lukasz.createam@gmail.com>
2021-06-03 00:39:12 +0200

    [skip ci] CI automation

--- build-init-container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -e
-
-docker build \
-  -f src/main/docker/initContainer.Dockerfile \
-  -t kilmajster/keycloak-username-password-attribute-authenticator:"$1" \
-  -t kilmajster/keycloak-username-password-attribute-authenticator:latest \
-  .
