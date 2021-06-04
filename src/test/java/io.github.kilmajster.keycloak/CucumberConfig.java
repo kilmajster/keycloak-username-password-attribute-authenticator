0cba309cf7fee7dd896c3391ccde1d306fd7259e
kilmajster <lukasz.createam@gmail.com>
2021-06-04 20:55:52 +0200

    Cucumber tests!

---
+++ src/test/java/io.github.kilmajster.keycloak/CucumberConfig.java
@@ -0,0 +1,10 @@
+package io.github.kilmajster.keycloak;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(features = "src/test/resources/cucumber")
+public class CucumberConfig {
+}
